1ce4536d3c9ad01b8cf7fbfc5b79a07f22aac609
DanielYyork <daniel.yyork@gmail.com>
2026-09-15 13:12:26 +0100

    Adding ability to load and remove openmm workflows.

--- gui/state.py
+++ gui/state.py
@@ -13,11 +13,19 @@ import streamlit as st
 SESSION_DEFAULTS = {
     "sequence": [],
     "preview_PHA": None,
+
+    # Current OpenMM workflow
     "openmm_steps": [],
+    "openmm_workflow_name": "Test",
+    "openmm_run_name": "Test",
+    "openmm_loaded_workflow_path": None,
+
+    # Generated OpenMM script
     "generated_openmm_script": None,
     "generated_openmm_script_path": None,
     "generated_openmm_system_name": None,
     "generated_openmm_system_type": None,
+    "generated_openmm_workflow_name": None,
 }
 
 
@@ -31,7 +39,6 @@ def initialise_session_state():
             st.session_state[key] = deepcopy(
                 default_value
             )
-
 
 def clear_polymer_sequence():
     """
@@ -51,13 +58,19 @@ def clear_generated_openmm_script():
     st.session_state.generated_openmm_script_path = None
     st.session_state.generated_openmm_system_name = None
     st.session_state.generated_openmm_system_type = None
+    st.session_state.generated_openmm_workflow_name = None
 
 
 def clear_openmm_workflow():
     """
-    Clear the workflow and generated-script information.
+    Clear the current OpenMM workflow and generated-script information.
     """
 
     st.session_state.openmm_steps = []
+
+    st.session_state.openmm_workflow_name = "Test"
+    st.session_state.openmm_run_name = "Test"
+
+    st.session_state.openmm_loaded_workflow_path = None
 
     clear_generated_openmm_script()
