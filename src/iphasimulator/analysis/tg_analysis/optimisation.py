78d78c3410dfa962ea79fefedc3308e3ae52db2f
DanielYyork <daniel.yyork@gmail.com>
2026-09-18 18:22:20 +0100

    Fixed import error.

--- src/iphasimulator/analysis/tg_analysis/optimisation.py
+++ src/iphasimulator/analysis/tg_analysis/optimisation.py
@@ -53,7 +53,7 @@ import pandas as pd
 from kneed import KneeLocator
 from sklearn.metrics import adjusted_rand_score
 
-from .descriptors import ChainDistanceDescriptors
+from ..descriptors import ChainDistanceDescriptors
 from ..pca import ChainPCAResult, calculate_chain_pca
 from .clustering import cluster_chain_pca
 
